f3610d27009b414b27c933027411bf2e77a65773
c18003981 <audrey.caressa@etu.univ-amu.fr>
2019-09-25 08:49:19 +0200

    ok

--- utils.inc.php
+++ utils.inc.php
@@ -19,5 +19,8 @@ function end_page()
 echo getenv('REMOTE_ADDR');
 // Correspond à l’adresse IP du serveur.
 echo getenv('HTTP_HOST');
+// Correspond au logiciel serveur Web utilisé.
+echo getenv('SERVER_SOFTWARE');
+
 
 ?>
