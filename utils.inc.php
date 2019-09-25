a26f0c87404fb016d960819d424b61f1efea23f9
c18003981 <audrey.caressa@etu.univ-amu.fr>
2019-09-25 08:43:02 +0200

    ok

---
+++ utils.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> 
+            <html lang="fr">
+                <head>
+                    <title>  Calculator  </title>
+                </head>
+                <body> <hr/><br/><strong>Test</strong><br/><hr/> '; //end of line
+}
+
+function end_page()
+{
+    echo '</body></html>';
+}
+
+// Correspond à l’adresse IP de celui qui visualise la page.
+echo getenv('REMOTE_ADDR');
+// Correspond à l’adresse IP du serveur.
+echo getenv('HTTP_HOST');
+
+?>
