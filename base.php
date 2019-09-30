c8d002099ead905083739bf282a8ae8200aedd39
c18003981 <audrey.caressa@etu.univ-amu.fr>
2019-09-30 09:23:08 +0200

    bd

--- base.php
+++ base.php
@@ -8,17 +8,25 @@ or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 mysqli_select_db($dbLink , dbBd)
 or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
 
-    //requêtes
-//$query = 'SELECT id, email, date FROM user';
+    requêtes
+$query = 'SELECT id, email, date FROM user';
 
-//envoi requêtes à la base de donnée
+envoi requêtes à la base de donnée
 
-//if(!($dbResult = mysqli_query($dbLink, $query)))
-//{
-//    echo 'Erreur de requête<br/>';
-//// Affiche le type d'erreur.
-//    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-//// Affiche la requête envoyée.
-//    echo 'Requête : ' . $query . '<br/>';
-//    exit();
-//}
+if(!($dbResult = mysqli_query($dbLink, $query)))
+{
+    echo 'Erreur de requête<br/>';
+// Affiche le type d'erreur.
+    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+    echo 'Requête : ' . $query . '<br/>';
+    exit();
+}
+
+
+while ($dbRow = mysqli_fetch_assoc($dbResult)) //on récupère les résultats de la requête {
+    echo $dbRow['id'] . '<br/>';
+    echo $dbRow['email'] . '<br/>';
+    echo $dbRow['date'] . '<br/>';
+    echo '<br/><br/>';
+}
