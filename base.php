ae0a15f0a5ceb3c3a92a9aefdb4e0d1a03fe3cb6
c18003981 <audrey.caressa@etu.univ-amu.fr>
2019-09-30 09:20:30 +0200

    bd

--- base.php
+++ base.php
@@ -9,4 +9,16 @@ mysqli_select_db($dbLink , dbBd)
 or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
 
     //requêtes
-$query = 'SELECT id, email, date FROM user';
+//$query = 'SELECT id, email, date FROM user';
+
+//envoi requêtes à la base de donnée
+
+//if(!($dbResult = mysqli_query($dbLink, $query)))
+//{
+//    echo 'Erreur de requête<br/>';
+//// Affiche le type d'erreur.
+//    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+//// Affiche la requête envoyée.
+//    echo 'Requête : ' . $query . '<br/>';
+//    exit();
+//}
